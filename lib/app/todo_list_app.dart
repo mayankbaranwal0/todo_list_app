5a9bb8c389b6381a6c93072f59f81d7267311d68
Mayank Baranwal <mb605508@gmail.com>
2025-05-05 01:58:22 +0530

    initial

---
+++ lib/app/todo_list_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TodoListApp extends StatelessWidget {
+  const TodoListApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'app/todo_list_app.dart';
+
+void main() {
+  runApp(const TodoListApp());
+}
